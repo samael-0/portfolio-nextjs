ff3768156881a1becc3b58d25a6f0a1c617f7acd
shrawan <mundushrm@gmail.com>
2025-12-14 22:20:45 +0545

    initialized version 2

---
+++ src/app/components/Header.tsx
@@ -0,0 +1,44 @@
+import { link } from "fs";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import React from "react";
+
+const url: { url: string; label: string }[] = [
+  {
+    url: "/",
+    label: "homepage",
+  },
+  {
+    url: "/blog",
+    label: "blog",
+  },
+  {
+    url: "/contact",
+    label: "contact",
+  },
+];
+
+export const Header = () => {
+  const currentPath = usePathname();
+  return (
+    // <ul className="flex gap-4 justify-center font-pop ">
+    //   <li>about</li>
+    //   <li>updates</li>
+    //   <li>contact</li>
+    // </ul>
+    <ul className="flex gap-4 justify-center ">
+      {url.map((links) => (
+        <li key={links.url}>
+          <Link
+            href={links.url}
+            className={`${
+              currentPath == links.url ? "border-b border-white" : ""
+            }`}
+          >
+            {links.label}
+          </Link>
+        </li>
+      ))}
+    </ul>
+  );
+};
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,14 +1,22 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { BBH_Sans_Bogle, Poppins, Roboto, Inter } from "next/font/google";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
+const poppins = Poppins({
+  variable: "--font-poppins",
+  weight: ["200"],
   subsets: ["latin"],
 });
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
+const inter = Inter({
+  variable: "--font-inter",
+  weight: ["200"],
+  subsets: ["latin"],
+});
+
+const roboto = Roboto({
+  variable: "--font-roboto",
+  weight: ["200"],
   subsets: ["latin"],
 });
 
@@ -25,8 +33,9 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${poppins.variable} ${roboto.variable} ${inter.variable} antialiased flex h-screen`}
       >
+        {/* <div className="font-">hello</div> */}
         {children}
       </body>
     </html>
